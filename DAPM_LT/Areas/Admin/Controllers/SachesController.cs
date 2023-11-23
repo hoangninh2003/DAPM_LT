f3eedf2279870118d98e61445564c8d8cff4e5fc
HaiBeo001 <128909350+HaiBeo001@users.noreply.github.com>
2023-11-23 18:56:16 +0700

    thêm sp

--- DAPM_LT/Areas/Admin/Controllers/SachesController.cs
+++ DAPM_LT/Areas/Admin/Controllers/SachesController.cs
@@ -86,17 +86,70 @@ namespace DAPM_LT.Areas.Admin.Controllers
         // POST: Admin/Saches1/Create
         // To protect from overposting attacks, enable the specific properties you want to bind to, for 
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public ActionResult Create([Bind(Include = "Idsach,Tieude,ImgSach,Tacgia,GiaMua,Namxuatban,Mota,Idloai")] Sach sach)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //        db.Saches.Add(sach);
+        //        db.SaveChanges();
+        //        return RedirectToAction("Index");
+        //    }
+
+        //    ViewBag.Idloai = new SelectList(db.Loais, "Idloai", "Tenloai", sach.Idloai);
+        //    return View(sach);
+        //}
+
         [HttpPost]
         [ValidateAntiForgeryToken]
         public ActionResult Create([Bind(Include = "Idsach,Tieude,ImgSach,Tacgia,GiaMua,Namxuatban,Mota,Idloai")] Sach sach)
         {
+            db.Saches.Add(sach);
             if (ModelState.IsValid)
             {
-                db.Saches.Add(sach);
-                db.SaveChanges();
-                return RedirectToAction("Index");
+                // Kiểm tra có thay đổi ảnh không
+                bool hasImageChanged = sach.ImageFile != null && sach.ImageFile.ContentLength > 0;
+
+                // Lấy đối tượng từ database để kiểm tra và giữ lại ảnh cũ (nếu có)
+                Sach existingTaiKhoan = db.Saches.Find(sach.Idsach);
+
+                if (existingTaiKhoan != null)
+                {
+                    // Giữ lại ảnh cũ nếu không có sự thay đổi
+                    if (!hasImageChanged)
+                    {
+                        sach.ImgSach = existingTaiKhoan.ImgSach;
+                    }
+
+                    // Cập nhật chỉ các trường cần thiết từ taiKhoan
+                    db.Entry(existingTaiKhoan).CurrentValues.SetValues(sach);
+
+                    // Nếu có thay đổi ảnh, cập nhật ảnh
+                    if (hasImageChanged)
+                    {
+                        byte[] imageData;
+                        using (Stream inputStream = sach.ImageFile.InputStream)
+                        {
+                            using (MemoryStream outputStream = new MemoryStream())
+                            {
+                                inputStream.CopyTo(outputStream);
+                                imageData = outputStream.ToArray();
+                            }
+                        }
+                        existingTaiKhoan.ImgSach = imageData;
+                    }
+                    
+                    db.SaveChanges();
+
+                    // Cập nhật lại session cho profile
+                    //Session["use"] = existingTaiKhoan;
+
+                    return RedirectToAction("Index", new { id = existingTaiKhoan.Idsach });
+                }
             }
 
+            // ModelState không hợp lệ hoặc có lỗi xảy ra, trả về view với dữ liệu hiện tại
             ViewBag.Idloai = new SelectList(db.Loais, "Idloai", "Tenloai", sach.Idloai);
             return View(sach);
         }
